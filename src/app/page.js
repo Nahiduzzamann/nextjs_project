2f2b495f24835c3c94c38dcc196699b7830cf6f0
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-12-21 12:13:56 +0600

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
-import Image from "next/image";
 
 const Home = () => {
   return (
-    <main className="">
-      <h1>Home</h1>
+    <main className="h-screen flex justify-center items-center">
+      <div className="max-w-md mx-auto mt-8 p-4 bg-gray-100 rounded-md">
+        <form>
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="inputField"
+          >
+            Type your question here...
+          </label>
+          <input
+            className="w-full p-2 border rounded-md"
+            type="text"
+            id="inputField"
+            name="inputField"
+            placeholder="Type question..."
+          />
+          <button
+            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </main>
   );
 };
